fc63e9497dc5e63774dd19fbde6435423afbdb0b
Eva Sraj <eva.sraj96@gmail.com>
2017-12-30 15:26:42 +0100

    2 packages v libraries

--- lib/libraries.r
+++ lib/libraries.r
@@ -9,6 +9,8 @@ library(shiny)
 library(httr)
 library(readxl)
 library(tidyr)
+library(sp)
+library(maptools)
 
 # Uvozimo funkcije za pobiranje in uvoz zemljevida.
 source("lib/uvozi.zemljevid.r", encoding = "UTF-8")
--- uvoz/uvoz.r
+++ uvoz/uvoz.r
@@ -4,13 +4,18 @@ sl <- locale("sl", decimal_mark = ",", grouping_mark = ".")
 
 # Funkcija, ki uvozi število dijakov glede na leto in vrsto štipendije
 uvozi.dijaki <- function() {
+  
   link <- "http://www.irssv.si/demografija-3/2013-01-11-18-12-48/2013-01-11-18-12-59/stevilo-dijakov-prejemnikov-stipendij-glede-na-vrsto-stipendije"
+  
   stran <- html_session(link,
                         add_headers("User-Agent" = "Mozilla/5.0 (X11; Ubuntu; Linux i686; rv:57.0) Gecko/20100101 Firefox/57.0"))
+  
   tabela <- stran %>% read_html() %>% html_nodes(xpath="//table") %>%
     .[[1]] %>% html_table(header = TRUE) %>% .[1:16, ] %>%
     sapply(parse_number, locale = locale(grouping_mark = " ")) %>% data.frame()
+  
   colnames(tabela) <- c("leto", "kadrovska", "drzavna", "zoisova", "druge")
+  
   return(tabela %>% melt(id.vars = "leto", variable.name = "stipendija", value.name = "stevilo"))
 }
 
@@ -18,7 +23,7 @@ uvozi.dijaki <- function() {
 dijaki <- uvozi.dijaki()
 
 
-# Funkcija, ki uvozi stevilo stipendistov glede na leto in vrsto stipendije
+# Funkcija, ki uvozi stevilo stipendistov glede na leto
 uvozi.stipendist <- function(){
   drzavne <- read_xlsx("podatki/stipendije_vse.xlsx", sheet = 1, range = "A1:F8")
   zoisove <- read_xlsx("podatki/stipendije_vse.xlsx", sheet = 2, range = "A19:F26")
@@ -33,11 +38,15 @@ uvozi.stipendist <- function(){
   zoisove$stipendija <- "zoisova"
   kadrovske.posredne$stipendija <- "posredna kadrovska"
   kadrovske.neposredne$stipendija <- "neposredna kadrovska"
+  
   stipendije.skupaj <- rbind(drzavne, zoisove, kadrovske.posredne, kadrovske.neposredne) %>%
     mutate(stipendija = factor(stipendija))
+  
   stipendije <- stipendije.skupaj %>% select(leto, stipendija, povprecna, sredstva)
+  
   stipendisti <- stipendije.skupaj %>% select(leto, stipendija, dijaki, studenti) %>%
     melt(id.vars = c("leto", "stipendija"), variable.name = "stipendisti", value.name = "stevilo")
+  
 }
 
 # Zapišimo podatke v razpredelnico stipendist
@@ -45,6 +54,7 @@ stipendisti <- uvozi.stipendist()
 
 # Funkcija, ki uvozi število štipendistov glede na pokrajine Slovenije
 uvozi.pokrajine <- function() {
+  
   data <- read_csv2("podatki/pokrajine.csv", trim_ws = TRUE, na = c("-", ""),
                     locale = locale(encoding = "Windows-1250"), skip = 6,
                     col_names = c("regija", "stipenditor", "stipendija", 2008:2011), n_max = 234) %>%
@@ -52,8 +62,11 @@ uvozi.pokrajine <- function() {
     melt(id.vars = c("regija", "stipenditor", "stipendija"),
          variable.name = "leto", value.name = "stevilo") %>%
     mutate(leto = parse_number(leto), stevilo = parse_number(stevilo))
+  
   return(data)
 }
+
+# Zapišimo podatke v razpredelnico pokrajine
 pokrajine <- uvozi.pokrajine()
 
 
